bf8d304991529520f66821501e7263271da1a198
Toghrul Gasimov <toghrulgasimov@gmail.com>
2019-01-28 19:34:06 +0100

    menu

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 let fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+   let s = fs.readFileSync("./public/menu.html") + "";
+   res.send(s);//test
+});
+router.get('/start', function(req, res, next) {
    let s = fs.readFileSync("./public/indexx.html") + "";
    res.send(s);//test
 });
-
 module.exports = router;
