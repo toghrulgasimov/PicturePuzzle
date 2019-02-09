a24db6f313273a8da962a88e2e8d24269da1df64
Toghrul Gasimov <toghrulgasimov@gmail.com>
2019-02-09 22:43:42 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,46 @@ const bcrypt = require('bcryptjs');
 const {ObjectID} = require('mongodb');
 const {mongoose} = require('../dao/mongoose');
 const {Contest} = require('../models/contest');
+const {PuzzlePlayer} = require('../models/player');
+const cheerio = require('cheerio');
 
-router.get('/', function (req, res) {
+router.get('/', async function (req, res) {
+    console.log("gettt");
+
+    if(req.query.id == undefined) return;
+    let user = await PuzzlePlayer.findOne({_id:req.query.id});
+    if(user == null) {
+        user = new PuzzlePlayer({_id:req.query.id, mission: 1});
+        user = await user.save();
+    }
+    console.log(user);
+    res.cookie('_id', user._id);
+    res.cookie('mission', user.mission);
     let s = fs.readFileSync("./public/menu.html") + "";
-    res.send(s);//test
+    const $ = cheerio.load(s);
+    $('#rinfo').text('Reytinq ' + user.mission);
+    res.send($.html());//test
+});
+
+router.post('/nextmission', async function (req, res) {
+    console.log("nextx mission post");
+    let _id = req.cookies._id;
+
+    let currMission = parseInt(req.cookies.mission);
+    currMission++;
+    res.cookie('mission', currMission);
+    await PuzzlePlayer.findOneAndUpdate({_id:_id}, {$set:{mission:currMission}});
+    res.send("succes");
 });
 
 router.get('/start', function (req, res) {
     let s = fs.readFileSync("./public/indexx.html") + "";
-    res.send(s);//test
+    const $ = cheerio.load(s);
+    $("#infoid").text(req.cookies._id);
+    $("#infoid").attr("mission", req.cookies.mission);
+    let mission = req.cookies.mission;
+    console.log(mission);
+    res.send($.html());//test
 });
 
 router.get('/contests', function (req, res) {
