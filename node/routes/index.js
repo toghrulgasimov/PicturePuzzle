01c81aa53a278cc0c26aaca3d816ec6ea3b92791
Toghrul Gasimov <toghrulgasimov@gmail.com>
2019-02-26 17:06:52 +0100

    update

--- node/routes/index.js
+++ node/routes/index.js
@@ -18,8 +18,12 @@ router.get('/', async function (req, res) {
     if (req.query._id == undefined) {
         // avto qeydiyyatdan kecmelidi
 
-        var ress = request('GET', 'http://35.231.39.26:3000/users/getuserinfo');
-        req.query = JSON.parse(ress.getBody());
+        //var ress = request('GET', 'http://35.231.39.26:3000/users/getuserinfo');
+        //req.query = JSON.parse(ress.getBody());
+        let s = fs.readFileSync("./public/menu2.html") + "";
+        const $ = cheerio.load(s);
+        //$('#info').attr("r", 'Reytinq ' + user.mission);
+        res.send($.html());//test
     }
     let user = await PuzzlePlayer.findOne({_id: req.query._id});
     if (user == null) {
